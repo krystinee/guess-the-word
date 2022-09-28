9311677af944b946232710b47038c4c48eb0ef3c
krystine_e <krysti.1527@gmail.com>
2022-09-28 10:23:35 -0500

    Elements and placeholders

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const guessedLetters = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess");
+const letterInput = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remainingGuesses = document.querySelector(".remaining");
+const span = document.querySelector(".remaining span");
+const message = document.querySelector(".message");
+const playAgainButton = document.querySelector(".play-again");
+
+const word = "magnolia";
+
+//Symbols displayed as placeholders for letters
+const placeholder = function (word) {
+    const placeholderLetters = [];
+    for (const letter of word) {
+        console.log(letter);
+        placeholderLetters.push("●");
+    }
+    wordInProgress.innerText = placeholderLetters.join("");
+};
+
+placeholder(word);
+
+guessButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    const guessInput = letterInput.value;
+    console.log(guessInput);
+    letterInput.value = "";
+});
